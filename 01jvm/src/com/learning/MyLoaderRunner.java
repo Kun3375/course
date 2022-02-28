f0bb4ad81a9835176fc37e83371e2ebe24a3aec1
Kun3375 <kun3375@qq.com>
2022-02-28 22:33:40 +0800

    避免打包后不可用，不用nio.Path

--- 01jvm/src/com/learning/MyLoaderRunner.java
+++ 01jvm/src/com/learning/MyLoaderRunner.java
@@ -1,11 +1,9 @@
 package com.learning;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Method;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 /**
  * @author kun
@@ -27,20 +25,27 @@ public class MyLoaderRunner extends ClassLoader {
         public Class<?> findClass(String name) throws ClassNotFoundException {
             // 约定特殊加载的后缀为 xlass；放在 classPath 下；无包名
             String suffix = ".xlass";
-            URL resource = getClass().getResource(name + suffix);
-            if (resource == null) {
-                throw new ClassNotFoundException("cant find class " + name);
+            try (
+                    InputStream resource = getClass().getResourceAsStream(name + suffix);
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream()
+            ) {
+                if (resource == null) {
+                    throw new ClassNotFoundException("cant find class " + name);
+                }
+                byte[] buffer = new byte[32];
+                int len;
+                while ((len = resource.read(buffer)) != -1) {
+                    baos.write(buffer, 0, len);
+                }
+                byte[] content = baos.toByteArray();
+                for (int i = 0; i < content.length; i++) {
+                    content[i] ^= 255;
+                }
+                return defineClass(name.substring(name.lastIndexOf("/") + 1), content, 0, content.length);
+            } catch (IOException e) {
+                throw new ClassNotFoundException("cant process class " + name);
             }
-            byte[] content;
-            try {
-                content = Files.readAllBytes(Paths.get(resource.toURI()));
-            } catch (IOException | URISyntaxException | NullPointerException e) {
-                throw new ClassNotFoundException("cant find class " + name);
-            }
-            for (int i = 0; i < content.length; i++) {
-                content[i] ^= 255;
-            }
-            return defineClass(name.substring(name.lastIndexOf("/") + 1), content, 0, content.length);
+
         }
 
     }
