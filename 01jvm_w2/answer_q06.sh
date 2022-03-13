70dd0b1e5e23c80657aa75fb699bf8f082159c92
Kun3375 <kun3375@qq.com>
2022-03-13 21:56:16 +0800

    fix name

--- 01jvm_w2/answer_q06.sh
+++ 01jvm_w2/answer_q06.sh
@@ -1,8 +1,13 @@
 #!/bin/zsh
 
+
+# netty server 在 netty/MyNettyServer
 mvn -f pom.xml clean package
+# netty client 在 netty/MyClient
 mvn -f pom2.xml clean package
+# 启动 server
 nohup java -jar target_server/server.jar &
 echo "\n--- sleep 3 for sever start---\n"
+# 请求 server
 java -jar target_client/client.jar
 echo "\n---remember kill server ps---\n"
